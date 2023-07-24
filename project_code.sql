a478b70334d08c7f33cb446ba48b1da06a11907d
yashjulka05 <135961038+yashjulka05@users.noreply.github.com>
2023-07-24 23:56:56 +0530

    Update project_code.sql

--- project_code.sql
+++ project_code.sql
@@ -1,3 +1,11 @@
+/*
+credit card Data Exploration 
+
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+
+*/
+
+
 --checking count match
 select count(*) from credit_card_transcations;
 
